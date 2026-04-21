133bc2faa57ea4a361d8d6d34a2e508d8f9b98ad
Kauan <kauanmonteirosilva789@gmail.com>
2026-04-21 13:27:11 -0300

    Atualização da função de notificação

--- notificacoes.py
+++ notificacoes.py
@@ -1,9 +1,16 @@
 from plyer import notification
-
-def enviar_notificacao(titulo, mensagem):
+import requests
+def enviar_notificacao(titulo, mensagem, user_id):
     notification.notify(
         title=titulo,
         message=mensagem,
         app_name="Minhas Tarefas",
         timeout=10
     )
+    try:
+        requests.post(
+            f"https://ntfy.sh/{user_id}-tarefas",
+            json={"title": titulo, "message": mensagem}
+        )
+    except Exception:
+        pass
--- utils.py
+++ utils.py
@@ -37,19 +37,26 @@ def verificar_prazo(tarefa_id, user_id, titulo, descricao, prazo, concluida, pra
     if agora > data_prazo:
         chave = f"{tarefa_id}_atrasada"
         if chave not in app.notificacoes_enviadas:
-            enviar_notificacao(titulo="Tarefa atrasada", mensagem=f"{titulo} está atrasada!")
+            enviar_notificacao(titulo="Tarefa atrasada", mensagem=f"{titulo} está atrasada!,",user_id=user_id)
             app.notificacoes_enviadas.add(chave)
-
+        return editar_task(
+            tarefa_id,
+            user_id,
+            titulo,
+            descricao,
+            prazo,
+            prazo_vencido=True
+        )
     elif 0 < minutos <= 15:
         chave = f"{tarefa_id}_15min"
         if chave not in app.notificacoes_enviadas:
-            enviar_notificacao(titulo="Lembrete de tarefa", mensagem=f"{titulo} vence em 15 minutos!")
+            enviar_notificacao(titulo="Lembrete de tarefa", mensagem=f"{titulo} vence em 15 minutos!",user_id=user_id)
             app.notificacoes_enviadas.add(chave)
 
     elif 15 < minutos <= 30:
         chave = f"{tarefa_id}_30min"
         if chave not in app.notificacoes_enviadas:
-            enviar_notificacao(titulo="Lembrete de tarefa", mensagem=f"{titulo} vence em 30 minutos!")
+            enviar_notificacao(titulo="Lembrete de tarefa", mensagem=f"{titulo} vence em 30 minutos!",user_id=user_id)
             app.notificacoes_enviadas.add(chave)
     else:
         return False
